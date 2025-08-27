d7ed0e7765c775877f510f6364af1d9a2c8aa9ad
TAINÁ ESTEFANI MARTINS <taina_e_martins@estudante.sesisenai.org.br>
2025-08-27 19:34:34 -0300

    feat: javascript na tela de cadastro + fix do javascript da tela de login

---
+++ src/JavaScript/Cadastro.js
@@ -0,0 +1,10 @@
+document.getElementById("cadastroForm").addEventListener("submit", function(event) {
+    event.preventDefault();
+
+const name = document.getElementById("name").value;
+const email = document.getElementById("email").value;
+const password = document.getElementById("password").value;
+
+
+
+});
--- src/JavaScript/Login.js
+++ src/JavaScript/Login.js
@@ -1,15 +1,19 @@
 document.getElementById("loginForm").addEventListener("submit", function(event) {
     event.preventDefault();
 
-    const user = document.getElementById("username").value.trim();
-    const pass = document.getElementById("password").value.trim();
+    const name = document.getElementById("name").value;
+    const email = document.getElementById("email").value;
+    const password = document.getElementById("password").value;
     const errorMessage = document.getElementById("error-message");
 
-    // Exemplo de validação (fixo só para demonstração)
-    if (user === "admin" && pass === "1234") {
-        alert("Login realizado com sucesso!");
-        window.location.href = "home.html"; // redireciona para outra página
+    const validEmail = "email";
+    const validPassword = "password";
+
+    if (email === validEmail && password === validPassword) {
+        alert("Login bem-sucedido!");
+        // window.location.href = "Home.html";
     } else {
-        errorMessage.textContent = "Usuário ou senha inválidos!";
+        alert("Nome de usuário ou senha incorretos.");
+        errorMessage.style.display = "block";
     }
 });
